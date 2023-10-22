1fa1f402187c56c522edef57331210fc5e2a999f
dahmed <dahmed@ncsu.edu>
2023-10-22 15:09:38 +0000

    Updated witencpy version

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="witencpy",
-    version="1.0.0",
+    version="1.0.1",
     author="David L. Adei",
     author_email="lokingdav@gmail.com",
     description="Witness Encryption based on BLS signatures in C++ (Python Bindings)",
